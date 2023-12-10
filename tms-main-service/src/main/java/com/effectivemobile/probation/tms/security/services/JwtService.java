e818de4964048dc6bfdc6faf3ac20932dc31dd66
Grayroof <1grayroof@gmail.com>
2023-12-11 01:31:55 +0300

    fix: set Value annotation to specific value instead of a variable

--- tms-main-service/src/main/java/com/effectivemobile/probation/tms/security/services/JwtService.java
+++ tms-main-service/src/main/java/com/effectivemobile/probation/tms/security/services/JwtService.java
@@ -16,10 +16,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-    @Value("${security.jwt.secret-key}")
+    @Value("3cfa76ef14937c1c0ea519f8fc057a80fcd04a7420f8e8bcd0a7567c272e007b")
     private String secretKey;
 
-    @Value("${security.jwt.expiration-time}")
+    @Value("3600000")
     private long jwtExpiration;
 
     public String extractUsername(String token) {
